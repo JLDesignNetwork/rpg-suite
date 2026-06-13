11dd41e0050c82c25437f98efa3c203d3ecb70df
JLDN Admin <59946223+JLDesignNetwork@users.noreply.github.com>
2026-06-13 17:12:51 +0200

    chore: remove temporary test and debug scripts

--- test3.js
+++
@@ -1,6 +0,0 @@
-const e = require('./lib/encounters');
-async function test() {
-  const result = await e.generateEncounter('', { system: 'dnd-5e', monsters: 'goblin=1' });
-  console.log(result.substring(0, 500));
-}
-test().catch(console.error);
